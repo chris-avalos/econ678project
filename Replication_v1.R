e988df108f243c8d5b6b686d2eae27bf451847ee
chris-avalos <cavalos96@gmail.com>
2025-04-16 19:13:54 -0400

    Update Replication_v1.R
    Added code to make first 3 columns of Table 2

--- Replication_v1.R
+++ Replication_v1.R
@@ -2,12 +2,17 @@ library(tidyr)
 library(dplyr)
 library(janitor)
 library(gt)
+library(stargazer)
 
 # Set up working director
 wd<-dirname(rstudioapi::getSourceEditorContext()$path)
 # Load data
 load(paste(wd, "REStatSubmission_FRB.RData", sep = "/"))
 #load("~/UMD/ECON 687/Project/REStatSubmission_FRB.RData")
+
+################################################################################
+# Table 1 - Summary Statistics
+################################################################################
 
 # Named list of labels and variable names
 labels <- c("2008 Delinquency Rate", "Center City", "Median House Value", "Median Age",
@@ -36,12 +41,48 @@ varss <- c("delrate", "CENTER", "CUML", "MEDAGE", "OVER65", "UNEMRT", "minpct",
 
 # Creates an empty data fram with two columns called "Variable" and "Mean"
 table.1 <- data.frame(Variable = character(), Mean = character())
-myData <- myData %>% filter(MINOF3==1 & L11==1)
 # Loop that goes through the vector varss and calculates mean for each variable and add to table.1
+myData <- myData %>% filter(MINOF3==1 & L11==1)
 for(i in 1:length(varss)){
-  mean <- weighted.mean(myData[[varss[i]]], w = myData$COUNT) %>% round(3)
+  mean <- weighted.mean(myData[[varss[i]]], w = myData$COUNT) %>% round(2)
   table.1[i,] <- c(labels[i], mean)
 }
 
 gt(table.1)
 gtsave(gt(table.1), "Table1.docx",wd)
+
+
+################################################################################
+# Table 2 - OLS Regression
+################################################################################
+
+# Model in column (1)
+table.2.1 <- lm(delrate ~ shr.dep.out_2001 + shr.dep.in_2001 + shr.aff.out_2001 + shr.aff.in_2001 + shr.cu_2001 + # Share of lending 
+                pur.dep.out_2001 + pur.dep.in_2001 + pur.aff.out_2001 + pur.aff.in_2001 + pur.cu_2001 + pur.ind_2001 + gse_2001 + # Purchases as share of lending 
+                SCORE2000 + relinc + relinc90 + UNEMRT + MEDAGE + OVER65 + CENTER + minpct + CUML + ownpct + vacpct + as.factor(MSA), # Baseline controls 
+                myData,
+                weights=COUNT)
+summary(table.2.1)
+
+# Model in column (2)
+table.2.2 <- lm(delrate ~ shr.dep.out_20046 + shr.dep.in_20046 + shr.aff.out_20046 + shr.aff.in_20046 + shr.cu_20046 + # Share of lending
+                pur.dep.out_20046 + pur.dep.in_20046 + pur.aff.out_20046 + pur.aff.in_20046 + pur.cu_20046 + pur.ind_20046 + gse_20046 + # Purchases as share of lending
+                SCORE2004 + relinc + relinc90 + UNEMRT + MEDAGE + OVER65 + CENTER + minpct + CUML + ownpct + vacpct + as.factor(MSA), # Baseline controls
+                myData,
+                weights=COUNT)
+summary(table.2.2)
+
+# Model in column (3)
+table.2.3 <- lm(delrate ~ shr.dep.out_20046 + shr.dep.in_20046 + shr.aff.out_20046 + shr.aff.in_20046 + shr.cu_20046 + # Share of lending
+                pur.dep.out_20046 + pur.dep.in_20046 + pur.aff.out_20046 + pur.aff.in_20046 + pur.cu_20046 + pur.ind_20046 + gse_20046 + # Purchases as share of lending
+                SCORE2004 + relinc + relinc90 + UNEMRT + MEDAGE + OVER65 + CENTER + minpct + CUML + ownpct + vacpct + as.factor(MSA) + # Baseline controls
+                hidti_20046 + hirate_20046 + pig_20046 + lowinc_20046 + midinc_20046 + zerinc_20046, # Additional Controls
+                myData,
+                weights=COUNT)
+summary(table.2.3)
+
+stargazer(table.2.1, table.2.2, table.2.3, 
+          type="text", 
+          title="Table 1. -- Delinquency Rate Estimations",
+          omit = c("SCORE2000", "SCORE2004", "SCORE2004", "relinc", "relinc90", "UNEMRT", "MEDAGE", "OVER65", "CENTER", "minpct", "CUML", "ownpct", "vacpct", "MSA", # Baseline controls
+          "hidti_20046", "hirate_20046", "pig_20046", "lowinc_20046", "midinc_20046", "zerinc_20046"))
